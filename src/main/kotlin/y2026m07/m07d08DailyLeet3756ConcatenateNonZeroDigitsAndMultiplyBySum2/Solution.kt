0fcb5cf4d1fcf91547732b2ce14d81a85c57c9fd
shineseong <hss275989@gmail.com>
2026-07-12 11:52:10 +0900

    refactor: replace Double-based operations with BigInteger for problem 3756

--- src/main/kotlin/y2026m07/m07d08DailyLeet3756ConcatenateNonZeroDigitsAndMultiplyBySum2/Solution.kt
+++ src/main/kotlin/y2026m07/m07d08DailyLeet3756ConcatenateNonZeroDigitsAndMultiplyBySum2/Solution.kt
@@ -1,7 +1,5 @@
 package y2026m07.m07d08DailyLeet3756ConcatenateNonZeroDigitsAndMultiplyBySum2
 
-import kotlin.math.pow
-
 class Solution {
     fun sumAndMultiply(
         s: String,
@@ -19,23 +17,26 @@ class Solution {
     private fun sumAndMultiPlyNonZero(nString: String): Int {
         val nonZeroDigits = nString
             .filter { it != '0' }
-            .map { it.digitToInt() }
 
-        val sum = nonZeroDigits.sum()
+        var sum = 0.toBigInteger()
+        nonZeroDigits.forEach { ch ->
+            sum = (sum + ch.digitToInt().toBigInteger()) % MOD
+        }
 
         val xSource = nonZeroDigits
-            .joinToString("")
             .ifEmpty { "0" }
 
-        val result = xSource.reversed().mapIndexed { index, ch ->
-            val number = ch.digitToInt() * 10.0.pow(index)
-            (number * sum) % MOD
-        }.sum()
+        var x = 0.toBigInteger()
+        xSource.forEach { ch ->
+            x = (x * 10.toBigInteger()) % MOD
+            x += ch.digitToInt().toBigInteger() * sum
+            x %= MOD
+        }
 
-        return (result % MOD).toInt()
+        return (x % MOD).toInt()
     }
 
     companion object {
-        private const val MOD = 1_000_000_000 + 7
+        private val MOD = (1_000_000_000 + 7).toBigInteger()
     }
 }
