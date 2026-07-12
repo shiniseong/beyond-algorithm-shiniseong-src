37f00c8e8a74d0405814025a2235a6d81fcc22d8
shineseong <hss275989@gmail.com>
2026-07-12 11:15:17 +0900

    fix: correct modulo operation precedence in problem 3756 calculation logic

--- src/main/kotlin/y2026m07/m07d08DailyLeet3756ConcatenateNonZeroDigitsAndMultiplyBySum2/Solution.kt
+++ src/main/kotlin/y2026m07/m07d08DailyLeet3756ConcatenateNonZeroDigitsAndMultiplyBySum2/Solution.kt
@@ -28,7 +28,7 @@ class Solution {
             .ifEmpty { "0" }
 
         val result = xSource.reversed().mapIndexed { index, ch ->
-            val number = ch.digitToInt() * (10.0.pow(index)) % MOD
+            val number = ch.digitToInt() * (10.0.pow(index) % MOD)
             (number * sum) % MOD
         }.sum()
 
