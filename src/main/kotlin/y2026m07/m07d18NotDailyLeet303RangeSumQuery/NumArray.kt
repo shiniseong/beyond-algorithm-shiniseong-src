5bc93a95e0a1503ee663a03ac67465afe5327678
shineseong <hss275989@gmail.com>
2026-07-18 14:16:42 +0900

    refactor: optimize 303 Range Sum Query using prefix sum approach

--- src/main/kotlin/y2026m07/m07d18NotDailyLeet303RangeSumQuery/NumArray.kt
+++ src/main/kotlin/y2026m07/m07d18NotDailyLeet303RangeSumQuery/NumArray.kt
@@ -1,10 +1,17 @@
 package y2026m07.m07d18NotDailyLeet303RangeSumQuery
 
-class NumArray(private val nums: IntArray) {
+class NumArray(nums: IntArray) {
+    private val prefixSum = IntArray(nums.size + 1)
+
+    init {
+        prefixSum[0] = 0
+        for (idx in 1..nums.size) {
+            prefixSum[idx] = prefixSum[idx - 1] + nums[idx - 1]
+        }
+    }
+
     fun sumRange(
         left: Int,
         right: Int,
-    ): Int = nums
-        .slice(left..right)
-        .sum()
+    ): Int = prefixSum[right + 1] - prefixSum[left]
 }
