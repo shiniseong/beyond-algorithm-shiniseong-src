72a9806e0a289b37e39f9a634deca5002bc7c937
shineseong <hss275989@gmail.com>
2026-08-02 19:30:45 +0900

    refactor: enhance commentary for 628 Max Product logic for clarity

--- src/main/kotlin/y2026m07/m07d26DailyLeet628MaxProductOfThreeNumbers/Solution.kt
+++ src/main/kotlin/y2026m07/m07d26DailyLeet628MaxProductOfThreeNumbers/Solution.kt
@@ -4,8 +4,11 @@ class Solution {
     fun maximumProduct(nums: IntArray): Int {
         nums.sort()
         val n = nums.size
-        val maxProduct = nums[n - 1] * nums[n - 2] * nums[n - 3]
-        val minProduct = nums[0] * nums[1] * nums[n - 1]
-        return maxOf(maxProduct, minProduct)
+        // 가장 큰 수의 세 곱
+        val case1 = nums[n - 1] * nums[n - 2] * nums[n - 3]
+        // 음수 두개와 양수 하나케이스를 고려한 곱
+        val case2 = nums[0] * nums[1] * nums[n - 1]
+        // 두 경우 중 최대값을 반환
+        return maxOf(case1, case2)
     }
 }
